402e18db411fc81bdd43442502cc3fd2226c2745
Ayush12 <openclass321@gmail.com>
2024-02-26 12:19:27 +0100

    Path Parameters

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "John",
+        "age": 26,
+        "class": "Years 12",
+    }
+}
+
 @app.get("/")
 def index():
-    return{"name":"First Data"}
+    return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(..., description="The ID of the student you want to view")):
+    if student_id in students:
+        return students[student_id]
+    else:
+        return {"error": "Student not found"}
